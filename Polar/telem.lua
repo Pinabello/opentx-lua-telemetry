43789afff9dcf1568654de25a2377b8dfb501c57
Paolo Luciani <pinabello@gmail.com>
2017-07-04 23:09:16 +0200

    Refactoring

--- Polar/telem.lua
+++ Polar/telem.lua
@@ -1,30 +1,46 @@
 
 local function run_func(event)
 
-  function round(num, numDecimalPlaces)
-    local mult = 10^(numDecimalPlaces or 0)
-    return math.floor(num * mult + 0.5) / mult
-  end
-  
-  lcd.clear()
-  
-  local batt_s = 'VFAS'
+local batt_s = 'VFAS'
   local batt_t = getValue(batt_s)
   local batt_v = {}
   local img_path = '/SCRIPTS/BMP/'
   local percent = 0
   local cell = 0
   local spacing = 2
+  local settings = getGeneralSettings()
+  local MEDIUM_SIZE = 20
   
   
-  local first_line = 5
+  local first_line = 6
   local second_line = 25
-  local third_line = 45
+  local third_line = 39
   
-  local onehalf_column = 45
+  local onehalf_column = 50
   local second_column = 70
+
+  function drawCurrentTime(x, y)
+    datetime = getDateTime()
+    lcd.drawPixmap(x, y, img_path .. 'clock.bmp')
+    lcd.drawText(x + 18, y + 5, format(datetime.hour)..":"..format(datetime.min)..":"..format(datetime.sec), SMLSIZE)
+  end
   
-  lcd.drawPixmap(6, 1, img_path .. 'battery.bmp')
+  function round(num, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+    return math.floor(num * mult + 0.5) / mult
+  end
+
+  function drawTxBattery(x,y)
+    percent = (getValue('tx-voltage') - settings.battMin) * 100 / (settings.battMax - settings.battMin)
+    lcd.drawRectangle(x, y -1, 15, 8)
+    lcd.drawFilledRectangle(x + 1, y-1 + 1, 13, 6, GREY_DEFAULT)
+    lcd.drawLine(x + 15, y-1 + 2, x + 15, y-1 + 5, SOLID, 0)
+    -- Tx Voltage
+    lcd.drawChannel(x + 20, y, 'RxBt', SMLSIZE)
+  end
+  
+  function lipoBattery(x,y)
+  lcd.drawPixmap(x, y, img_path .. 'battery.bmp')
   batt_v[2] = { low = 7.4, high = 8.4 }
   batt_v[3] = { low = 11.1, high = 12.6 }
   batt_v[4] = { low = 14.8, high = 16.8 }
@@ -35,40 +51,69 @@ local function run_func(event)
     -- Only show voltage and cell count if battery is connected
     batt_c = math.ceil(batt_t / 4.25)
     percent = (batt_t - batt_v[batt_c]['low']) * (100 / (batt_v[batt_c]['high'] - batt_v[batt_c]['low']))
-    lcd.drawChannel(4, 55, batt_s, LEFT)
-    lcd.drawText(lcd.getLastPos() + spacing, 55, batt_c .. 'S', 0)
     cell = batt_t / batt_c
-    lcd.drawText(lcd.getLastPos() + spacing, 55, round(cell, 1) .. 'v', 0)
+    if cell < 3.7 then
+          lcd.drawText(4, 55, round(batt_t,1)..'v', BLINK)
+          lcd.drawText(lcd.getLastPos() + spacing, 55, round(cell, 1) .. 'v', BLINK)
+    else 
+          lcd.drawText(4, 55, round(batt_t,1)..'v', LEFT)
+          lcd.drawText(lcd.getLastPos() + spacing, 55, round(cell, 1) .. 'v', 0)
+    end      
+    lcd.drawText(lcd.getLastPos() + spacing, 55, batt_c .. 'S', 0)
   end
   
   if percent > 0 then
     local myPxX = math.floor(percent * 0.37)
     local myPxY = 11 + 37 - myPxX
-    lcd.drawFilledRectangle(13, myPxY, 21, myPxX, FILL_WHITE)
+    lcd.drawFilledRectangle(x + 7, myPxY, 21, myPxX, FILL_WHITE)
   end
   
   for i = 36, 2, -2 do
-    lcd.drawLine(14, 11 + i, 32, 11 + i, SOLID, GREY_DEFAULT)
+    lcd.drawLine(x + 7, y + 10 + i, x + 27, y + 10 + i, SOLID, GREY_DEFAULT)
   end
   
+  end
+  
+  function format(num)
+    formatted = ''..num
+    if num < 10 then
+      formatted = '0'..num
+    end
+    return formatted
+  end
+  
+  lcd.clear()
+  lipoBattery(2,1)
   -- Time
-  datetime = getDateTime()
-  lcd.drawText(onehalf_column, first_line, datetime.hour..":"..datetime.min..":"..datetime.sec, 0)
+  
+  drawCurrentTime(onehalf_column - 10, first_line -5)
+
+  drawTxBattery(onehalf_column + 70, first_line)
+  
   
   -- Flight Mode
 
   if getValue(MIXSRC_SA) < 0 then
-    lcd.drawText(second_column + 15, third_line, 'Level', MIDSIZE)
+    lcd.drawText(second_column, second_line, 'Normal Mode', MEDIUM_SIZE)
   elseif getValue(MIXSRC_SA) >= 0 then
-    lcd.drawText(second_column  + 15, third_line, 'Acro', MIDSIZE)
+    lcd.drawText(second_column + 2, second_line, 'Acro Mode', MEDIUM_SIZE)
   end
   
   -- Arm Status
-  if getValue(MIXSRC_SF) > 0 then
-    lcd.drawText(second_column, second_line, 'ARMED', MIDSIZE)
+  if getValue(MIXSRC_SF) > 0 and getValue(MIXSRC_SC) > 0 then
+    lcd.drawText(second_column + 7, third_line, 'Armed', MIDSIZE + BLINK)
   else
-    lcd.drawText(second_column, second_line, 'DISARMED', MIDSIZE)
+    lcd.drawText(second_column, third_line, 'Disarmed', MIDSIZE)
   end
+  
+  -- RSSI Data
+  if getValue('RSSI') > 0 then
+    percent = round(((math.log(getValue('RSSI') - 28, 10) - 1) / (math.log(72, 10) - 1)) * 100, -1)
+  else
+    percent = 0
+  end
+  lcd.drawPixmap(164, 1, img_path .. 'rssi_' .. percent .. '.bmp')
+  lcd.drawText(182, 55, getValue('RSSI')..'db', SMLSIZE)
   
 end
 
