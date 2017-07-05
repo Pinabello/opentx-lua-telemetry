e319a4e5e37055d21822a527d5b39e879847b6ab
Paolo Luciani <pinabello@gmail.com>
2017-07-05 21:33:23 +0200

    Small adjustaments

--- Polar/telem.lua
+++ Polar/telem.lua
@@ -1,7 +1,8 @@
 
 local function run_func(event)
 
-local batt_s = 'VFAS'
+  local batt_s = 'VFAS'
+
   local batt_t = getValue(batt_s)
   local batt_v = {}
   local img_path = '/SCRIPTS/BMP/'
@@ -10,12 +11,9 @@ local batt_s = 'VFAS'
   local spacing = 2
   local settings = getGeneralSettings()
   local MEDIUM_SIZE = 20
-  
-  
   local first_line = 6
   local second_line = 25
   local third_line = 39
-  
   local onehalf_column = 50
   local second_column = 70
 
@@ -24,7 +22,7 @@ local batt_s = 'VFAS'
     lcd.drawPixmap(x, y, img_path .. 'clock.bmp')
     lcd.drawText(x + 18, y + 5, format(datetime.hour)..":"..format(datetime.min)..":"..format(datetime.sec), SMLSIZE)
   end
-  
+
   function round(num, numDecimalPlaces)
     local mult = 10^(numDecimalPlaces or 0)
     return math.floor(num * mult + 0.5) / mult
@@ -38,42 +36,42 @@ local batt_s = 'VFAS'
     -- Tx Voltage
     lcd.drawChannel(x + 20, y, 'RxBt', SMLSIZE)
   end
-  
+
   function lipoBattery(x,y)
-  lcd.drawPixmap(x, y, img_path .. 'battery.bmp')
-  batt_v[2] = { low = 7.4, high = 8.4 }
-  batt_v[3] = { low = 11.1, high = 12.6 }
-  batt_v[4] = { low = 14.8, high = 16.8 }
-  batt_v[5] = { low = 18.5, high = 21.0 }
-  batt_v[6] = { low = 22.2, high = 25.2 }
-  
-  if batt_t > 3 then
-    -- Only show voltage and cell count if battery is connected
-    batt_c = math.ceil(batt_t / 4.25)
-    percent = (batt_t - batt_v[batt_c]['low']) * (100 / (batt_v[batt_c]['high'] - batt_v[batt_c]['low']))
-    cell = batt_t / batt_c
-    if cell < 3.7 then
-          lcd.drawText(4, 55, round(batt_t,1)..'v', BLINK)
-          lcd.drawText(lcd.getLastPos() + spacing, 55, round(cell, 1) .. 'v', BLINK)
-    else 
-          lcd.drawText(4, 55, round(batt_t,1)..'v', LEFT)
-          lcd.drawText(lcd.getLastPos() + spacing, 55, round(cell, 1) .. 'v', 0)
-    end      
-    lcd.drawText(lcd.getLastPos() + spacing, 55, batt_c .. 'S', 0)
+    lcd.drawPixmap(x, y, img_path .. 'battery.bmp')
+    batt_v[2] = { low = 7.4, high = 8.4 }
+    batt_v[3] = { low = 11.1, high = 12.6 }
+    batt_v[4] = { low = 14.8, high = 16.8 }
+    batt_v[5] = { low = 18.5, high = 21.0 }
+    batt_v[6] = { low = 22.2, high = 25.2 }
+
+    if batt_t > 3 then
+      -- Only show voltage and cell count if battery is connected
+      batt_c = math.ceil(batt_t / 4.25)
+      percent = (batt_t - batt_v[batt_c]['low']) * (100 / (batt_v[batt_c]['high'] - batt_v[batt_c]['low']))
+      cell = batt_t / batt_c
+      if cell < 3.7 then
+        lcd.drawText(9, 56, round(batt_t,1)..'v', BLINK + SMLSIZE)
+        lcd.drawText(lcd.getLastPos() + spacing + 2, 56, round(cell, 1) .. 'v', BLINK + SMLSIZE)
+      else
+        lcd.drawText(9, 56, round(batt_t,1)..'v', LEFT + SMLSIZE)
+        lcd.drawText(lcd.getLastPos() + spacing + 2, 56, round(cell, 1) .. 'v', SMLSIZE)
+      end
+      lcd.drawText(lcd.getLastPos() + spacing + 2, 56, batt_c .. 's', SMLSIZE)
+    end
+
+    if percent > 0 then
+      local myPxX = math.floor(percent * 0.37)
+      local myPxY = 11 + 37 - myPxX
+      lcd.drawFilledRectangle(x + 7, myPxY, 21, myPxX, FILL_WHITE)
+    end
+
+    for i = 36, 2, -2 do
+      lcd.drawLine(x + 7, y + 10 + i, x + 27, y + 10 + i, SOLID, GREY_DEFAULT)
+    end
+
   end
-  
-  if percent > 0 then
-    local myPxX = math.floor(percent * 0.37)
-    local myPxY = 11 + 37 - myPxX
-    lcd.drawFilledRectangle(x + 7, myPxY, 21, myPxX, FILL_WHITE)
-  end
-  
-  for i = 36, 2, -2 do
-    lcd.drawLine(x + 7, y + 10 + i, x + 27, y + 10 + i, SOLID, GREY_DEFAULT)
-  end
-  
-  end
-  
+
   function format(num)
     formatted = ''..num
     if num < 10 then
@@ -81,16 +79,15 @@ local batt_s = 'VFAS'
     end
     return formatted
   end
-  
-  lcd.clear()
-  lipoBattery(2,1)
-  -- Time
-  
-  drawCurrentTime(onehalf_column - 10, first_line -5)
 
-  drawTxBattery(onehalf_column + 70, first_line)
-  
-  
+  lcd.clear()
+  lipoBattery(1,1)
+
+  drawCurrentTime(onehalf_column -11, first_line -5)
+
+  -- drawTxBattery(onehalf_column + 70, first_line)
+
+
   -- Flight Mode
 
   if getValue(MIXSRC_SA) < 0 then
@@ -98,14 +95,14 @@ local batt_s = 'VFAS'
   elseif getValue(MIXSRC_SA) >= 0 then
     lcd.drawText(second_column + 2, second_line, 'Acro Mode', MEDIUM_SIZE)
   end
-  
+
   -- Arm Status
   if getValue(MIXSRC_SF) > 0 and getValue(MIXSRC_SC) > 0 then
     lcd.drawText(second_column + 7, third_line, 'Armed', MIDSIZE + BLINK)
   else
     lcd.drawText(second_column, third_line, 'Disarmed', MIDSIZE)
   end
-  
+
   -- RSSI Data
   if getValue('RSSI') > 0 then
     percent = round(((math.log(getValue('RSSI') - 28, 10) - 1) / (math.log(72, 10) - 1)) * 100, -1)
@@ -113,8 +110,8 @@ local batt_s = 'VFAS'
     percent = 0
   end
   lcd.drawPixmap(164, 1, img_path .. 'rssi_' .. percent .. '.bmp')
-  lcd.drawText(182, 55, getValue('RSSI')..'db', SMLSIZE)
-  
+  lcd.drawText(182, 56, getValue('RSSI')..'db', SMLSIZE)
+
 end
 
 return { run=run_func}
